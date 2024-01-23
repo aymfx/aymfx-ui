331f51cbc3a1a3f63592d95e1df0470226cdccc2
aymfx <aymfx@qq.com>
2024-01-23 10:58:48 +0800

    feat: 完成icon组件

--- docs/components/icons/index.vue
+++ docs/components/icons/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="demo-icon-list">
-    <div class="icon" v-for="icon in ElementPlusIconsVue">
+    <div class="icon" v-for="icon in ElementPlusIconsVue" @click="copy(icon)">
       <component :is="icon" style="width: 30px"></component>
       <span>{{ icon.name }}</span>
     </div>
@@ -9,6 +9,12 @@
 
 <script setup>
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import { useCopy } from '@/packages/hooks/useCopy.ts';
+import { toLine } from '@/packages/utils';
+
+const copy = (icon) => {
+  useCopy(`<au-icon${toLine(icon.name)}></au-icon${toLine(icon.name)}>`);
+};
 </script>
 
 <style lang="scss">
@@ -32,10 +38,15 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue';
     height: 100px;
     border-right: 1px solid #dcdfe6;
     border-bottom: 1px solid #dcdfe6;
+    cursor: pointer;
 
     span {
       margin: 5px;
     }
   }
+}
+svg {
+  width: 2em;
+  height: 2em;
 }
 </style>
